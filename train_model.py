f3f69a9c8b38bb6e066f60a3c5ee09f7492ad6fb
2022BCS0102-prathima-sindhu-varshini <prathima7754@gmail.com>
2026-04-13 17:01:33 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+
+
+df = pd.read_csv("winequality-red.csv")
+
+X = df.drop("quality", axis=1)
+y = df["quality"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("Model trained and saved as model.pkl")
